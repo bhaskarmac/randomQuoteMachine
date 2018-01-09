70adc300c78a7ba64bc7626d779ab381ccac0c6c
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-09 16:25:19 +0530

    Adding new server URL from forismatic

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,20 @@ $(document).ready(function () {
 	getRandomQuote();
 
 	function getRandomQuote() {
-		$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(quoteData) {
-			console.log('quoteData=>', quoteData);
-			$('.quote').html(quoteData[0].content);
+		$.ajax({
+			url: "https://api.forismatic.com/api/1.0/?",
+			dataType: "jsonp",
+			data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+			success: function(response) {
+				console.log('response=>', response);
+			}
 		});
+		
 	}
+
+	$('#btnRandomQuote').click(function () {
+		console.log('btnRandomQuote clicked=>');
+		getRandomQuote();
+	});
 
 });
